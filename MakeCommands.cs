9c61ea9ac96848840634dae9d61114d604823f30
michal rybarik <michal.rybarik@gmail.com>
2022-06-30 10:59:21 +0200

    init commit

---
+++ MakeCommands.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+
+namespace Wox.Plugin.ShareX
+{
+    internal static class MakeCommand
+    {
+        private static void RunShareXCommandLine(string argument)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = Paths.ShareXAppPath;
+            process.StartInfo.Arguments = "-" + argument;
+            process.Start();
+        }
+
+        internal static Result CaptureRegionScreenshot()
+        {
+            return new Result
+            {
+                Title = "capture region",
+                SubTitle = "screenshot",
+                Score = 50,
+                IcoPath = "Images\\region-screenshot.png",
+                Action = e =>
+                {
+                    RunShareXCommandLine("RectangleRegion");
+                    return true;
+                }
+            };
+        }
+
+        internal static Result ShowColorPicker()
+        {
+            return new Result
+            {
+                Title = "color picker",
+                SubTitle = "color picker",
+                Score = 50,
+                IcoPath = "Images\\color-picker.png",
+                Action = e =>
+                {
+                    RunShareXCommandLine("ColorPicker");
+                    return true;
+                }
+            };
+        }
+
+        internal static Result ShowScreenColorPicker()
+        {
+            return new Result
+            {
+                Title = "screen color picker",
+                SubTitle = "color picker",
+                Score = 50,
+                IcoPath = "Images\\screen-color-picker.png",
+                Action = e =>
+                {
+                    RunShareXCommandLine("ScreenColorPicker");
+                    return true;
+                }
+            };
+        }
+
+        internal static Result ShowRuler()
+        {
+            return new Result
+            {
+                Title = "ruler",
+                SubTitle = "ruler tool",
+                Score = 50,
+                IcoPath = "Images\\ruler.png",
+                Action = e =>
+                {
+                    RunShareXCommandLine("Ruler");
+                    return true;
+                }
+            };
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿
+using System.Collections.Generic;
+using mroot_lib;
+
+namespace Wox.Plugin.ShareX
+{
+    static class Paths
+    {
+        public static string ShareXAppPath => mroot.get_enviro_var_value("sharex");
+    }
+
+    public class WoxVPNExtension : IPlugin
+    {
+        #region members
+
+        private PluginInitContext _context;
+
+        #endregion
+
+        #region wox overrides
+
+        public void Init(PluginInitContext context)
+        {
+            _context = context;
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> resultList = new List<Result>();
+
+            AddScreenshotCommand(resultList, query);
+            AddColorPickerCommand(resultList, query);
+            AddScreenColorPicker(resultList, query);
+            AddRuller(resultList, query);
+
+            return resultList;
+        }
+
+        #endregion
+
+        #region commands
+
+        private void AddScreenshotCommand(List<Result> resultList, Query query)
+        {
+            if (StringTools.IsEqualOnStart(query.Search, "screenshot", "capture", "regionscreenshot"))
+            {
+                resultList.Add(MakeCommand.CaptureRegionScreenshot());
+            }
+        }
+
+        private void AddColorPickerCommand(List<Result> resultList, Query query)
+        {
+            if (StringTools.IsEqualOnStart(query.Search, "colorpicker"))
+            {
+                resultList.Add(MakeCommand.ShowColorPicker());
+            }
+        }
+
+        private void AddScreenColorPicker(List<Result> resultList, Query query)
+        {
+            if (StringTools.IsEqualOnStart(query.Search, "colorpicker-screen"))
+            {
+                resultList.Add(MakeCommand.ShowScreenColorPicker());
+            }
+        }
+        private void AddRuller(List<Result> resultList, Query query)
+        {
+            if (StringTools.IsEqualOnStart(query.Search, "ruler"))
+            {
+                resultList.Add(MakeCommand.ShowRuler());
+            }
+        }
+
+        #endregion
+
+    }
+}
